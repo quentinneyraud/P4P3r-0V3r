8fe72539929c1318104590eb773d804ba9337459
Agustina <geminis_agus@hotmail.com>
2016-10-11 15:33:12 +0200

    Create Login and Register fragments

--- app/src/main/java/fr/quentinneyraud/www/p4p3r0v3r/AccountActivity.java
+++ app/src/main/java/fr/quentinneyraud/www/p4p3r0v3r/AccountActivity.java
@@ -1,17 +1,60 @@
 package fr.quentinneyraud.www.p4p3r0v3r;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AccountActivity extends AppCompatActivity {
 
     static final String TAG = "AccountActivity";
 
+    private FirebaseAuth fireAuth;
+    private FirebaseAuth.AuthStateListener fireAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        fireAuth = FirebaseAuth.getInstance();
+
+        //Listen changes on authentication state
+        fireAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getEmail());
+                    // TODO : Redirect to conversations page
+                    // Intent intent = new Intent(MainActivity.this, UserProfileActivity.class);
+                    // startActivity(intent);
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+
+            }
+        };
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        fireAuth.addAuthStateListener(fireAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (fireAuthListener != null) {
+            fireAuth.removeAuthStateListener(fireAuthListener);
+        }
+    }
+
 }
---
+++ app/src/main/java/fr/quentinneyraud/www/p4p3r0v3r/LoginFragment.java
@@ -0,0 +1,38 @@
+package fr.quentinneyraud.www.p4p3r0v3r;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LoginFragment extends Fragment {
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+}
