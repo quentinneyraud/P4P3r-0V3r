f5fd73ef33ae058e0e8ad323521523824dd015c9
Agustina <geminis_agus@hotmail.com>
2016-10-13 16:24:50 +0200

    Update splash screen

--- app/src/main/java/fr/quentinneyraud/www/p4p3r0v3r/SplashScreenActivity.java
+++ app/src/main/java/fr/quentinneyraud/www/p4p3r0v3r/SplashScreenActivity.java
@@ -22,7 +22,6 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
 
         BusProvider.getInstance().register(this);
 
