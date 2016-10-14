0cdd46ae3a87e1f6cea95612845b5b3af3b83507
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-10-14 08:13:52 +0200

    clear getUser events

--- app/src/main/java/fr/quentinneyraud/www/p4p3r0v3r/User/events/getUserCancelledEvent.java
+++ app/src/main/java/fr/quentinneyraud/www/p4p3r0v3r/User/events/getUserCancelledEvent.java
@@ -6,13 +6,13 @@ import fr.quentinneyraud.www.p4p3r0v3r.utils.AbstractErrorEvent;
  * Created by quentin on 14/10/2016.
  */
 
-public class getUserCancelledEvent extends AbstractErrorEvent {
+public class GetUserCancelledEvent extends AbstractErrorEvent {
 
-    public getUserCancelledEvent() {
+    public GetUserCancelledEvent() {
         super();
     }
 
-    public getUserCancelledEvent(String errorMessage) {
+    public GetUserCancelledEvent(String errorMessage) {
         super(errorMessage);
     }
 }
