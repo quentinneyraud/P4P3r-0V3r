0da736dafac8a699e5704febbdccb85074b3e000
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-10-21 05:46:42 +0200

    hide pseudo & time in conversation when same user speak multiple times

--- app/src/main/java/fr/quentinneyraud/www/p4p3r0v3r/Message/MessageAdapter.java
+++ app/src/main/java/fr/quentinneyraud/www/p4p3r0v3r/Message/MessageAdapter.java
@@ -1,9 +1,12 @@
 package fr.quentinneyraud.www.p4p3r0v3r.Message;
 
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -20,6 +23,7 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
     private static final String TAG = "MessageAdapter";
     private List<Message> messageList;
     private HashMap<String, String> userList = new HashMap<>();
+    private String lastUserUid = "";
 
     public MessageAdapter(List<Message> messages) {
         messageList = messages;
@@ -42,9 +46,20 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
     public void onBindViewHolder(final ViewHolder holder, int position) {
         Message message = messageList.get(position);
 
-        holder.getTimeTextView().setText(message.getFormattedDate("HH:mm"));
-        holder.getAuthorTextView().setText(userList.get(message.getUserUid()));
+        // hide pseudo & time if same user speak multiple times
+        if (lastUserUid.equals(message.getUserUid())) {
+            FrameLayout.LayoutParams linearLayoutParams = new FrameLayout.LayoutParams(RecyclerView.LayoutParams.WRAP_CONTENT, RecyclerView.LayoutParams.WRAP_CONTENT);
+            linearLayoutParams.setMargins(0, 0, 0, 0);
+            holder.getInfosLinearLayout().setVisibility(View.GONE);
+            holder.getTextTextView().setLayoutParams(linearLayoutParams);
+        } else {
+            holder.getTimeTextView().setText(message.getFormattedDate("HH:mm"));
+            holder.getAuthorTextView().setText(userList.get(message.getUserUid()));
+        }
+
         holder.getTextTextView().setText(message.getMessage());
+
+        lastUserUid = message.getUserUid();
     }
 
     public void addMessage(Message message) {
@@ -64,6 +79,8 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
         TextView authorTextView;
         @BindView(R.id.fragment_message_time)
         TextView timeTextView;
+        @BindView(R.id.fragment_message_infos)
+        LinearLayout infosLinearLayout;
 
         public TextView getTextTextView() {
             return textTextView;
@@ -75,6 +92,10 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
 
         public TextView getTimeTextView() {
             return timeTextView;
+        }
+
+        public LinearLayout getInfosLinearLayout() {
+            return infosLinearLayout;
         }
 
         public ViewHolder(View view) {
